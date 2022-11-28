d2e8c31ee311e15680452e95770dc9b6addc1099
liaoyongjun <sobadname@gmail.com>
2022-11-28 10:19:48 +0800

    初始化

---
+++ src/test/java/com/tykj/guidebook/GuidebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tykj.guidebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
